7ceed1bd353a891de668649da34d9fb5fc603c4f
ddorn <diego.dorn@free.fr>
2025-01-30 16:52:05 +0100

    feat(vue): template for vue+neutralino project

---
+++ vue/src/main.ts
@@ -0,0 +1,27 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Neutralino from '@neutralinojs/lib'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+window.Neutralino = Neutralino
+
+;(async () => {
+    if  (!window.NL_PORT) {
+        const config = await import('./auth_info.json')
+        window.NL_PORT = config.port
+        window.NL_TOKEN = config.accessToken
+    }
+    Neutralino.init()
+})()
+
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
