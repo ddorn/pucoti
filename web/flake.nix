da7229cb6636d1855ec8a3095c5b0d753b65a56c
Félix Dorn <git@felixdorn.fr>
2025-05-07 11:58:49 +0200

    feat: cleanup web

--- web/flake.nix
+++ web/flake.nix
@@ -5,7 +5,7 @@
     let pkgs = nixpkgs.legacyPackages."x86_64-linux";
     in {
       devShells."x86_64-linux".default = pkgs.mkShell {
-        nativeBuildInputs = [ pkgs.nodejs_23 pkgs.pnpm ];
+        nativeBuildInputs = [ pkgs.nodejs_23 pkgs.pnpm pkgs.static-web-server ];
       };
     };
 }
